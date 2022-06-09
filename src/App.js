eb6d74c3b0a32cd3d3d3b27cf55850ce72c311e7
Haidarali Masu <haidaralimasu123@gmail.com>
2022-06-09 23:09:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { MoralisProvider } from "react-moralis";
 const App = () => {
   return (
     <MoralisProvider
-      serverUrl="https://wybbjccy07if.usemoralis.com:2053/server"
-      appId="j0jsTExJ1XfRhIn1ujvv9JrQyJfsL2R95aFe70IA"
+      serverUrl="https://1k9wu6uoelqz.usemoralis.com:2053/server"
+      appId="THWOGlPxREbcdxNKWniuFfGQbnsWQN4FieQ1VrQI"
     >
       <Router />
     </MoralisProvider>
--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./details.css";
+// import "./details.css";
 
 const Detail = (props) => {
   return (
